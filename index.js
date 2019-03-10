6a4cc05fe0794eebfe9e69d2c5a4908e24531cfa
ANNEMARIE OSULLIVAN <annemarieos.irl@gmail.com>
2019-03-10 13:01:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(kittens){
+  var kittens = ["Julie", "Otis", "Garfield"]
+  return kittens
+}
